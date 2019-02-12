2dcef7d1a83368cd9df0d0335fc40e20a14231b4
Róbert Tóth <robert.toth.ext@nokia.com>
2019-02-12 16:50:23 +0100

    animation, form validation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="hello">
-    {{ name }}
-    {{ btnState ? 'The button is disabled' : 'The button is active'}}
-    <button class="button is-primary" v-on:click="changeName" v-bind:disabled="btnState">Change name</button>
-    <!-- <button class="button is-primary" v-bind:class="{'is-loading' : bgColor}" v-on:click="changeName">Change name</button> -->
+   
     <div class="holder">
+      <form @submit.prevent="addSkill">
+          <input type="text" placeholder="Enter a skill you have"  v-model="skill" v-validate="'min: 5'" name="skill">
+          <!-- errors object is part of veevalidate-->
+          <transition name="alert-in">
+             <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+          </transition>
+         
+      </form>
+
       <ul>
         <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
-      <p v-if="skills.length >=1">You have more than 1 skills</p>
-      <p v-else>You have less skills.</p>
-      <!-- <div v-bind:class="{ alert: showAlert, 'another-class' : showClass}"></div>  -->
-      <div v-bind:class="alertObject"></div> 
-      <div v-bind:style="{backgroundColor: bgColor, width: bgWidth, height: bgHeight}"></div>
+      <p>These are the skills that you possess.</p>
     </div>
   </div>
 </template>
@@ -22,36 +24,91 @@ export default {
   name: 'Skills',
   data() {
     return {
-      // name: 'Coursetro',
-      // btnState: true,
+      skill: '',
       skills: [
         {"skill" :"Vue.js"},
         {"skill" :"Frontend dev"}
-      ],
-      alertObject: {
-        alert: true,
-        "another-class": true
-      },
-
-      bgColor: 'yellow',
-      bgWidth: '100%',
-      bgHeight: '30px'
-      // showAlert: true,
-      // showClass: true
+      ]
+    }
+  },
+  methods: {
+    addSkill(){
+      this.$validator.validateAll().then((result) => {
+        if (result){
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        } else {
+          console.log("Not valid")
+        }
+      }) 
     }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .holder {
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
   .alert {
-    background-color: yellow;
-    width :100%;
-    height: 30px;
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
   }
 
-  .another-class {
-    border: 1px solid black;
+  @keyframes bounce-in {
+  0% {
+    transform: scale(0);
   }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+i {
+  float:right;
+  cursor:pointer;
+}
 </style>
